539cfd93f99bed1214c39621f40b1afcdffaddc3
Sam Brightman <sam.brightman@gmail.com>
2017-01-31 17:03:45 +0100

    Add writing to performance testing, allow running directly with external profiler

--- vcf/test/prof.py
+++ vcf/test/prof.py
@@ -3,10 +3,14 @@ import cProfile
 import timeit
 import pstats
 import sys
+import os
 
 def parse_1kg():
-    for line in vcf.Reader(filename='vcf/test/1kg.vcf.gz'):
-        pass
+    in_vcf = vcf.Reader(filename='vcf/test/1kg.vcf.gz')
+    with open(os.devnull, "w") as fh:
+        out_vcf = vcf.Writer(fh, template=in_vcf)
+        for line in in_vcf:
+            out_vcf.write_record(line)
 
 if len(sys.argv) == 1:
     sys.argv.append(None)
@@ -29,5 +33,7 @@ elif sys.argv[1] == 'stat':
     finally:
         statprof.stop()
         statprof.display()
+elif sys.argv[1] == 'run':
+    parse_1kg()
 else:
     print 'prof.py profile/time'
